3c07de93b94caa5785b4566420025f7c5a65b248
alkresin <alkresin@debian.my>
2025-01-27 19:49:59 +0300

    minor fix

--- build.sh
+++ build.sh
@@ -15,9 +15,9 @@ export HWGUI_LIB=$HB_ROOT/hwgui-code/hwgui/lib
 $HRB_EXE hwbc -n -i$HRB_INC -i$HWGUI_INC -D__GUI -es2
 gcc hwbc.c -ohwb -I $HRB_INC -L $HRB_LIB -L $HWGUI_LIB -Wl,--start-group $HWGUI_LIBS $HARBOUR_LIBS -Wl,--end-group `pkg-config --cflags gtk+-2.0` `pkg-config gtk+-2.0 --libs` $SYSTEM_LIBS
 
-rm hwb.c
+rm hwbc.c
 
 $HRB_EXE hwbc -n -i$HRB_INC -i$HWGUI_INC -es2
 gcc hwbc.c -ohwbc -I $HRB_INC -L $HRB_LIB -Wl,--start-group $HARBOUR_LIBS -Wl,--end-group $SYSTEM_LIBS
 
-rm hwb.c
+rm hwbc.c
