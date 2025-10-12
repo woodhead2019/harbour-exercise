4f1fe994c23cfce37230339ca7ad2673a813ab24
woodhead2019 <woodhead2019@users.noreply.github.com>
2025-10-12 13:32:02 +0800

    删除 hwbuilder 下已废弃的同步脚本与说明

--- hwbuilder/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-git checkout upstream-master
-git fetch upstream && git merge upstream/master
-git checkout hwbuilder-subtree
-git merge upstream-master
-git subtree push --prefix=hwbuilder origin hwbuilder-subtree
-echo ">>> 现在去 GitHub 把 hwbuilder-subtree → main 合并即可"
