32eaf063c37c75d79695734d514aed383725e683
woodhead2019 <woodhead2019@users.noreply.github.com>
2025-10-22 22:13:03 +0800

    modify build-dos-end.sh

--- build-dos-env.sh
+++ build-dos-env.sh
@@ -11,7 +11,8 @@ qemu-img create -f raw dos.img 2000M
 ls -lh dos.img
 
 log "===== 2. 下载 FreeDOS 1.4 LiveCD ====="
-wget -q --show-progress -O FD14-LiveCD.zip \
+#wget -q --show-progress -O FD14-LiveCD.zip
+wget -q -O FD14-LiveCD.zip \
   https://www.ibiblio.org/pub/micro/pc-stuff/freedos/files/distributions/1.4/FD14-LiveCD.zip
 unzip -q FD14-LiveCD.zip   # 得到 FD14BOOT.img + FD14LIVE.iso
 ls -lh FD14BOOT.img FD14LIVE.iso
@@ -36,7 +37,7 @@ while IFS= read -r cmd; do
 done < keys.txt | qemu-system-i386 -m 16 -drive file=dos.img,format=raw -cdrom FD14LIVE.iso -boot d -nographic
 '
 log "===== 4. 首次从硬盘启动 ====="
-timeout 15s qemu-system-i386 -m 16 -drive file=dos.img,format=raw -boot c \
+timeout 35s qemu-system-i386 -m 16 -drive file=dos.img,format=raw -boot c \
   -nographic -serial stdio <<'EOF' || true
 ver
 echo FreeDOS is alive!
